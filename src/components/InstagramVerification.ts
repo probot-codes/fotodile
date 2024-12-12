03ba225059a0f4019d71af66a484c18aae3eb927
probot-codes <ps25082005@gmail.com>
2024-12-12 11:50:19 +0530

    Build added

---
+++ src/components/InstagramVerification.ts
@@ -0,0 +1,13 @@
+// verification.ts
+
+export interface InstagramProfileInfo {
+  username: string;
+  full_name: string;
+  biography: string;
+  profile_pic_url: string;
+  is_private: boolean;
+  num_posts: number;
+  num_followers: number;
+  num_follows: number;
+  external_url: string;
+}
